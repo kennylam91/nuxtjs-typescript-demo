d3969dadd6a411e149ce3a7ec60f605830817f9a
kennylam91 <phamvanlam1991@gmail.com>
2020-11-23 16:56:28 +0700

    first commit , working

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>About</h3>
+    <p>{{ name }}</p>
+    <button @click="increase">Increase</button>{{ count }}
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator";
+@Component({
+
+})
+export default class extends Vue {
+  name: string = "pham van lam";
+  count: number = 0;
+
+  increase() {
+    this.count++;
+  }
+}
+</script>
